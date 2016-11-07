8d8e141fdd0a4b88bbc2c4d31d4f682082a9eed6
venkateshwarluk <ksrinuvenkatesh@gmail.com>
2016-11-07 14:54:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Raj";
+var age = "25 Years";
+var dateOfBirth = "05 JAN 1990";
+var placeOfBirth = "Bengalore";
+
+console.log(name);
+console.log(age);
+console.log(dateOfBirth);
+console.log(placeOfBirth);
